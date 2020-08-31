cb527579abd6261d02620d03e5f9cfc6d0485851
coyotewind <coyoteswind@gmail.com>
2020-08-30 21:00:04 -0400

    build

---
+++ build/precache-manifest.a993a6490ebaa11511759a3e7d82b59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b528e1071aa605f775c66e3b312a0c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73fc5343e0280bc1565a",
+    "url": "/static/css/main.24e370cb.chunk.css"
+  },
+  {
+    "revision": "4b678c239f18d985e0bc",
+    "url": "/static/js/2.b28ca8da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b28ca8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73fc5343e0280bc1565a",
+    "url": "/static/js/main.6a0070c9.chunk.js"
+  },
+  {
+    "revision": "767a7513c6a547ff2677",
+    "url": "/static/js/runtime-main.6063b6f5.js"
+  }
+]);
